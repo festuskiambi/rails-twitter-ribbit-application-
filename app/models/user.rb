6f2537ce7e7ac1ee6a329a4dfd262b1adba336cf
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-04-28 10:02:29 +0300

    generated the session controller"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
-
+before_save :create_avatar_url
 validates :name, presence: true
 validates :username, uniqueness: true, presence: true
 validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\Z/}
-
+def user_params
+      params.require(:user).permit(:avatar_url,:name,:username, :email, :password, :password_confirmation)
+    end
+def create_avatar_url
+    self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
+end
+private
+ 
+def prep_email
+    self.email = self.email.strip.downcase if self.email
+end
 end
